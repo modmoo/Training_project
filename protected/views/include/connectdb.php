d18f794b1dd4943c99e79137e6ad541918b7fc28
niwath <dev.niwath@gmail.com>
2014-12-24 20:41:48 +0700

    แก้ include connectdb config main รหัสผ่าน

--- protected/views/include/connectdb.php
+++ protected/views/include/connectdb.php
@@ -1,6 +1,6 @@
 <?php
 $username = "root";
-$password = "";
+$password = "admin";
 $hostname = "localhost"; 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) or die(mysql_error());
